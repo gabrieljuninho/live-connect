2cc912f1c454a47b8eaf6a85248376fc05b73e8e
gabrieljuninho <gabrieltobing789@gmail.com>
2024-12-22 19:03:04 +0700

    feat: setup middleware for body parsing and CORS handling

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 
 import { logger } from "./utils/logger";
-
-dotenv.config();
-
-const app: Application = express();
 
 if (!process.env.APP_PORT) {
   process.exit(1);
 }
 
 const PORT = process.env.APP_PORT!;
+
+dotenv.config();
+
+const app: Application = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+
+  next();
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
